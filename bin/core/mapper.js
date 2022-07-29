acd3736b07f54aa76f57e969f86a8a3046b8ac29
Jean Menezes <jeanvbonimenezes@gmail.com>
2022-07-29 10:41:24 -0300

    fix: ajust mapper

--- bin/core/mapper.js
+++ bin/core/mapper.js
@@ -12,33 +12,36 @@ function mapCypressTags(fileName, htmlContent) {
 
   if (!tags)
     console.log(chalk.red(`No tags detected in ${fileName} html file`));
+  else {
+    tags.forEach((tag) => {
+      const tagLocator = tag.match(LOCATOR_TAG_PROP_REGEX);
+      const tagDataCy = tag.match(DATA_CY_TAG_PROP_REGEX);
 
-  tags.forEach((tag) => {
-    const tagLocator = tag.match(LOCATOR_TAG_PROP_REGEX);
-    const tagDataCy = tag.match(DATA_CY_TAG_PROP_REGEX);
+      if (tagDataCy) {
+        const tagDataCySelector = `[${tagDataCy[0]}]`;
 
-    if (tagDataCy) {
-      if (tagLocator) {
-        const locatorName = tagLocator[0]
-          .replace('data-locator="', "")
-          .replace('"', "'")
-          .replace("'", "");
+        if (tagLocator) {
+          const locatorName = tagLocator[0]
+            .replace('data-locator="', "")
+            .replace('"', "'")
+            .replace("'", "");
 
-        jsonLocator[locatorName] = tagDataCy[0]
-          .replace('"', "'")
-          .replace('"', "'");
-      } else {
-        const locatorName = tagDataCy[0]
-          .replace('data-test="', "")
-          .replace('"', "'")
-          .replace("'", "");
+          jsonLocator[locatorName] = tagDataCySelector
+            .replace('"', "'")
+            .replace('"', "'");
+        } else {
+          const locatorName = tagDataCy[0]
+            .replace('data-test="', "")
+            .replace('"', "'")
+            .replace("'", "");
 
-        jsonLocator[locatorName] = tagDataCy[0]
-          .replace('"', "'")
-          .replace('"', "'");
+          jsonLocator[locatorName] = tagDataCySelector
+            .replace('"', "'")
+            .replace('"', "'");
+        }
       }
-    }
-  });
+    });
+  }
 
   return jsonLocator;
 }
