6f5b15047a73b4ea641b4bceb18d5613cc0f09b4
Jean <jeanvbonimenezes@gmail.com>
2022-09-14 10:30:40 -0400

    add spinners

--- bin/commands/detail.js
+++ bin/commands/detail.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const chalk = require("chalk");
 const glob = require("glob");
 const { exec } = require("child_process");
+const { createSpinner } = require("nanospinner");
 
 function saveDetailsWith(locator) {
   fs.mkdir("./locator-details", (err) => {
@@ -51,11 +52,15 @@ function detail() {
 
     files.forEach((file) => {
       exec(`node ${file}`, function (err) {
+        const spinner = createSpinner(`Detailing ${file} locators`);
+
         if (err) {
           console.log(chalk.red("Failed to run customize files"));
 
           process.exit(1);
         }
+
+        spinner.success();
       });
     });
   });
