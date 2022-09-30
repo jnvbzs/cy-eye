09f18dd9f63354cb057b1d3406fe40757748d3df
Jean <jeanvbonimenezes@gmail.com>
2022-09-29 22:02:10 -0400

    detach detail lib

--- bin/commands/detail.js
+++ bin/commands/detail.js
@@ -4,44 +4,6 @@ const glob = require("glob");
 const { exec } = require("child_process");
 const { createSpinner } = require("nanospinner");
 
-function saveDetailsWith(locator) {
-  fs.mkdir("./locator-details", (err) => {
-    if (err) {
-      if (err.code !== "EEXIST") {
-        console.log(chalk.red("Failed to create locator-details directory"));
-
-        process.exit(1);
-      }
-    }
-
-    fs.mkdir(`./locator-details/${locator["context"]}`, (err) => {
-      if (err) {
-        if (err.code !== "EEXIST") {
-          console.log(chalk.red("Failed to create locator context directory"));
-
-          process.exit(1);
-        }
-      }
-
-      const jsFileContent = `
-      export const ${locator["context"]} = ${JSON.stringify(locator)}
-      `;
-
-      fs.writeFile(
-        `./locator-details/${locator["context"]}/locators.js`,
-        jsFileContent,
-        (err) => {
-          if (err) {
-            console.log(chalk.red("Failed to create snippet js locator file"));
-
-            process.exit(1);
-          }
-        }
-      );
-    });
-  });
-}
-
 function detail() {
   glob("./**/*.customize.js", function (err, files) {
     if (err) {
@@ -67,6 +29,5 @@ function detail() {
 }
 
 module.exports = {
-  saveDetailsWith,
   detail,
 };
